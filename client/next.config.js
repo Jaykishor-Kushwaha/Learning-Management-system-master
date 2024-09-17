2f143f3df1ed93177a3d5217b6a1cda6e16e4362
Jaykishor <jaykishorkushwaha009@gmail.com>
2024-09-17 17:59:26 +0530

    Initial commit

---
+++ client/next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['res.cloudinary.com','randomuser.me'],
+      }, reactStrictMode: true,
+     
+}
+
+
+module.exports = nextConfig;
+
+
+module.exports = nextConfig
